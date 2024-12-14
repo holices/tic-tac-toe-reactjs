b43f54fa2474b45e846d0fa0ae3c06318716218e
Holices Junior <dev.holices@gmail.com>
2024-12-14 23:09:57 +0000

    build: created players component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
+
 export function Player({ name, symbol }) {
+  const [playerName, setPlayerName] = useState(name);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function renderPlayerName() {
+    const handleChange = (event) => {
+      setPlayerName(event.target.value);
+    }
+    return isEditing ? (
+        <input 
+          type="text" 
+          required 
+          value={playerName} 
+          onChange={handleChange} 
+        />
+      ) : (
+        <span className="player-name">{playerName}</span>
+      )
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {renderPlayerName()}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={() => setIsEditing((editing => !editing))}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   )
 }
